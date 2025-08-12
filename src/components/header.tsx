47acdd6a3e3c52be0350d4de5494de67ac108ab4
aleckurser <aleckurser@gmail.com>
2025-08-12 23:47:42 +0530

    Add new files and folders

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="bg-white shadow p-4 flex justify-between items-center">
+      <h2 className="text-xl font-semibold text-gray-800">Pixelcraft Studio</h2>
+      <nav>
+        <a href="#" className="text-blue-500 hover:underline">Home</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
