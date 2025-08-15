458a0bf9c7c19aa2f8eb20e375b68ae0d5e94d38
aleckurser <aleckurser@gmail.com>
2025-08-15 07:09:18 +0530

    Add new files and folders

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App' // මේක homepage එක වෙන්න පුළුවන්
-import FeaturesPage from './pages/FeaturesPage' // මේක features පිටුව
-import AboutPage from './pages/AboutPage' // මේක about පිටුව
+import App from './App' // ඔබේ Homepage එක
+import FeaturesPage from './pages/FeaturesPage' // Features පිටුව
+import AboutPage from './pages/AboutPage' // About පිටුව
+import LoginPage from './pages/login' // ඔබේ Login පිටුව
 import './index.css' // Tailwind CSS styles
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -13,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/features" element={<FeaturesPage />} />
         <Route path="/about" element={<AboutPage />} />
-        {/* ඔබට තවත් pages අවශ්‍ය නම්, මෙතනට Route එකක් දාන්න පුළුවන් */}
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
