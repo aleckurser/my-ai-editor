641f4999b62544b6c1a1c638f2ab4f2993292901
aleckurser <aleckurser@gmail.com>
2025-08-15 21:58:43 +0530

    Add new files and folders

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,10 +8,15 @@ module.exports = {
         fadeIn: {
           '0%': { opacity: 0, transform: 'translateY(10px)' },
           '100%': { opacity: 1, transform: 'translateY(0)' }
-        }
+        },
+        'logo-spin': { // logo spin animation එකේ keyframes මෙතනට දානවා
+          from: { transform: 'rotate(0deg)' },
+          to: { transform: 'rotate(360deg)' },
+        },
       },
       animation: {
-        fadeIn: 'fadeIn 0.5s ease-out forwards'
+        fadeIn: 'fadeIn 0.5s ease-out forwards',
+        'logo-spin': 'logo-spin infinite 20s linear', // animation එක මෙතනට දානවා
       }
     }
   },
